6f62c0514a2867c2840c0e445eb6419187493a28
Muhammad_Haris <hariscool987@gmail.com>
2025-12-12 05:14:23 -0800

    basics/snack_selector.py
    This is a beginner-level Python project that takes user input for their preferred snack and checks if it is available.
    It demonstrates:
    
    Using input() to get user data
    
    Conditional statements (if-else)
    
    String formatting with f-strings
    
    Skills Practiced:
    
    Python fundamentals
    
    User interaction
    
    Logical conditions

---
+++ 2_snack.py
@@ -0,0 +1,6 @@
+snack=input("Enter your preferred snacks:")
+
+if snack == "cookies" or snack =="samosa":
+    print(f"Good choice! we'll serve you {snack}")
+else:
+    print(f"sorry we dont have {snack}")
