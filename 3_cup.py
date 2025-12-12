84f58d2d7db21a80ed7c6af8d5d1a0c445dce69e
Muhammad_Haris <hariscool987@gmail.com>
2025-12-12 05:15:36 -0800

    basics/cup_size_checker.py
    Project Name: Cup Size Price Checker
    Category: Python Basics / User Input / Conditional Statements
    
    Description:
    This beginner-level Python project takes user input for a cup size and outputs its corresponding price.
    It demonstrates:
    
    Using input() to get user data
    
    Conditional statements (if-elif-else)
    
    Converting input to lowercase with .lower() for consistency
    
    Skills Practiced:
    
    Python fundamentals
    
    Logical conditions with multiple branches
    
    Handling invalid input

---
+++ 3_cup.py
@@ -0,0 +1,13 @@
+cup = input("Choose your cup size(small/medium/large):").lower()
+if cup == "small":
+    print("price is 10 rupees")
+elif cup == "medium":
+    print("Price is 15")
+elif cup == "large":
+    print("price of 20 rupees")
+else:
+    print("invlid size")
+    
+
+    
+    
